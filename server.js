1413b9c5698c9248d6bc50d2b2068c6791285393
mw_aldi <aldi.prasetio@machtwatch.co.id>
2022-02-22 11:24:58 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const { gql } = require('apollo-server');
+
 const typeDefs = gql`
     type Query{
         greeting: String
     }
 `;
 
-console.log(typeDefs);
+// console.log(typeDefs);
+
+const resolvers = {
+    Query: {
+        greeting: () => `Hello Anu!`
+    }
+};
+
+const server = new ApolloServer({typeDefs,resolvers});
+server.listen({port: 9000})
+    .then(({url}) => console.log(`Server running at ${url}`));
